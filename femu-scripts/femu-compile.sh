f7b8b2bb5b41513654d92840e82334e098f6fbd5
wurikiji <wurikiji@gmail.com>
2022-03-28 23:45:46 -0700

    SQLITE compile option added

--- femu-scripts/femu-compile.sh
+++ femu-scripts/femu-compile.sh
@@ -5,8 +5,8 @@ NRCPUS="$(cat /proc/cpuinfo | grep "vendor_id" | wc -l)"
 make clean
 # --disable-werror --extra-cflags=-w
 ../configure --enable-kvm --target-list=x86_64-softmmu --disable-git-update --disable-werror \
-  --extra-cflags=-DSQLITE_OMIT_LOAD_EXTENSION \
-  --extra-cxxflags=-DSQLITE_OMIT_LOAD_EXTENSION
+  --extra-cflags="-DSQLITE_OMIT_LOAD_EXTENSION -DSQLITE_OMIT_DEPRECATED -DSQLITE_OMIT_SHARED_CACHE"
+
 make -j $NRCPUS
 
 echo ""
---
+++ hw/block/femu/sas/sas-vfs.c
@@ -0,0 +1,10 @@
+#include "sqlite3.h"
+
+#define SAS_VFS_NAME "sas-vfs"
+#define MIN_BYTE_TO_WRITE 512
+#define
+
+typedef struct sas_vfs sas_vfs;
+struct sas_vfs {
+  sqlite3_vfs base;
+};
